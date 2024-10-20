18a145c2669693624642efc029555eff8142e67c
kabilan-s <kabilanselvakumar313@gmail.com>
2024-10-20 21:32:53 +0530

    redux setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1 className='text-red-400 text-center'>Hello</h1>
+      <h1 className='text-red-400 text-center'>welcome</h1>
     </div>
   );
 }
---
+++ frontend/src/redux/store.js
@@ -0,0 +1,12 @@
+import {combineReducers, configureStore} from '@reduxjs/toolkit'
+import ProductReducer from './slices/productsSlice'
+
+const reducer = combineReducers({
+    ProductsState: ProductReducer
+})
+
+const store = configureStore({
+    reducer,
+})
+
+export default store
